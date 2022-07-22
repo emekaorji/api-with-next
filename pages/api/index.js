d603cd8048d8021477aab664302329ba8e74a15c
Emeka Orji <emekapraiseo@gmail.com>
2022-07-22 21:57:15 +0100

    First Commit

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import fruits from "../../json/fruits.json";
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+	res.status(200).json(fruits);
 }
